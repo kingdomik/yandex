973265a2d5c30ed6a398b864dfd4ab79f129a853
kingdomik <dnsk@mail.ru>
2015-02-16 22:34:29 +0300

    Added matchers as separate files

---
+++ src/main/java/ru/yandex/common/matchers/IsEqualWithDelta.java
@@ -0,0 +1,36 @@
+package ru.yandex.common.matchers;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
+
+
+public class IsEqualWithDelta extends TypeSafeDiagnosingMatcher<LocalTime> {
+	
+    private final LocalTime time;
+    private final ChronoUnit unit;
+    private final int delta;
+
+    public IsEqualWithDelta(LocalTime time, ChronoUnit unit, int delta) {
+        this.time = time;
+        this.unit = unit;
+        this.delta = delta;
+    }
+
+    @Override
+    public void describeTo(final Description description) {
+       description
+       	.appendText("date should be ")
+       	.appendValue(time)
+       	.appendText(" with delta " + delta + " " + unit);
+    }
+
+    @Override
+    protected boolean matchesSafely(final LocalTime item, final Description mismatchDescription) {
+       mismatchDescription.appendText(" was ").appendValue(item);
+       return unit.between(item, time) <= delta;
+    }
+
+}
